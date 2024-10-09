c0afd39c2ef3a67f7a0504d426dbf1658a3b677c
DingoDevYT <landonian2006@gmail.com>
2024-10-09 20:38:02 +1100

    BIG UPGRADE FOR THE PORTFOLIO
    title says it all :D

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const typingTextElement = document.getElementById('typing-text');
-    const text = 'print("Hello Internet!");';
+    const typingTextElement1 = document.getElementById('typing-text1');
+    const text1 = "Console.WriteLine('Hello Traveller.');\nConsole.WriteLine('Welcome to my website!');";
 
     function typeWriter(text, element, i, callback) {
         if (i < text.length) {
-            element.innerHTML += text.charAt(i);
+            if (text.charAt(i) === '\n') {
+                element.innerHTML += '<br>';
+            } else {
+                element.innerHTML += text.charAt(i);
+            }
             i++;
-            setTimeout(() => typeWriter(text, element, i, callback), 100);
-        } else if (callback) {
-            setTimeout(callback, 1000);
+            setTimeout(() => typeWriter(text, element, i, callback), 20);
+        } else {
+            if (callback) {
+                setTimeout(callback, 500);
+            }
         }
     }
 
-    typeWriter(text, typingTextElement, 0);
+    function fadeInContent() {
+        const contentElements = document.querySelectorAll('.content');
+        contentElements.forEach(element => {
+            element.classList.add('fade-in');
+        });
+    }
+
+    typeWriter(text1, typingTextElement1, 0, fadeInContent);
+
+    // Add hover effect for game icons
+    const gameIcons = document.querySelectorAll('.game-icon img');
+
+    gameIcons.forEach(img => {
+        const staticSrc = img.src;
+        const gifSrc = img.getAttribute('data-gif'); 
+
+        img.addEventListener('mouseenter', () => {
+            img.src = gifSrc;
+        });
+
+        img.addEventListener('mouseleave', () => {
+            img.src = staticSrc;
+        });
+    });
+
 });
